7602aea9fbfc4f9268b986e34585c107970734b6
Md Reayd Hossain <billion.market.world@gmail.com>
2026-07-03 21:16:19 +0600

    fix: disable bundle manifest caching for vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,13 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
-        //
+        // Vercel-এর রিড-অনলি ফাইল সিস্টেমের জন্য রানটাইম ক্যাশ রাইট বন্ধ করা
+        $this->app->useBootstrapCachePath('/tmp/bootstrap/cache');
+        
+        // ম্যানুয়ালি প্যাকেজ ও সার্ভিস ক্যাশ ফাইল লোড করা বন্ধ করা
+        $this->app->instance('manifest.package', null);
     }
 
     /**
